5f8167fd7bc68e0798ed3e3160f82261603d6aa8
Ivan Popov <ma1n.py@yandex.ru>
2024-09-25 23:36:07 +0300

    revert static

--- backend/app/main.py
+++ backend/app/main.py
@@ -20,6 +20,7 @@ def configure_application(
         show_error_details=settings.app.show_error_details,
     )
 
+    app.serve_files("app/static")
     configure_error_handlers(app)
     configure_authentication(app, settings)
     configure_docs(app, settings)
