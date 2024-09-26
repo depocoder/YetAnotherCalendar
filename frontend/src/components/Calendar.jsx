cb2c8e0ebe1d194f09da44640db842a6712b9040
Azamat <86872713+AzamatKomaev@users.noreply.github.com>
2024-09-26 14:11:05 +0300

    add frontend, run it with docker compose (#12)

---
+++ frontend/src/components/Calendar.jsx
@@ -0,0 +1,19 @@
+import flatpickr from "flatpickr";
+import { useCallback, useRef } from "react";
+
+function Calendar() {
+    const fp1 = useRef();
+
+    const inputRef = useCallback((node) => {
+      if (node !== null) {
+        fp1.current = flatpickr(node, {
+            enableTime: true,
+            dateFormat: "Y-m-d H:i",
+        });
+      }
+    }, []);
+
+    return (<input type="date" ref={inputRef} />);
+}
+
+export default Calendar;
