924d7710e7eea7018ca39d5da0a4b4ac54456565
Ivan Popov <ma1n.py@yandex.ru>
2024-09-27 21:18:33 +0300

    Fix Azamat's docker-compose.yaml

--- backend/yet_another_calendar/settings.py
+++ backend/yet_another_calendar/settings.py
@@ -36,7 +36,7 @@ class Settings(BaseSettings):
     # quantity of workers for uvicorn
     workers_count: int = 1
     # Enable uvicorn reloading
-    reload: bool = True
+    reload: bool = env.bool("YET_ANOTHER_CALENDAR_RELOAD", False)
 
     # Current environment
     environment: str = "dev"
