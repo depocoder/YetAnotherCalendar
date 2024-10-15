a87231a48161c37048bc80ed00b39a7a651ad369
Ivan <60302361+depocoder@users.noreply.github.com>
2024-10-15 19:56:11 +0300

    mini release 1.1.2
    * feat: change axios requests
    
    * fix: change page login
    
    * feat: add context, getNetologyCourse, bulkEvents for calendar
    
    * fix: fetchCourseAndEvents email, password
    
    * styled: add calendar component
    
    * chore: del folders, files,
    
    * styled: add calendar component
    
    * fix change route calendar
    
    * fix ci
    
    * fix frontend issues
    
    * fix: rename varable .env
    
    * Timezone big update
    
    * linter fixes
    
    
    ---------
    
    Co-authored-by: alex karpovich <aakarpovich@mtt.ru>
    Co-authored-by: KytakBR <gusmanov2006@gmail.com>
    Co-authored-by: Карпович Александр <alieksandr.karpovich@mail.ru>
    Co-authored-by: AzamatKomaev <azamatkomaev15@gmail.com>

---
+++ frontend/src/App.jsx
@@ -0,0 +1,71 @@
+import React, {useState} from 'react';
+import {BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom';
+import LoginRoute from './pages/LoginRoute';
+import CalendarRoute from './pages/CalendarRoute';
+import {loginModeus, searchModeus} from './services/api';
+import PrivateRoute from "./components/Calendar/PrivateRoute"; // Ваши API-запросы
+
+const App = () => {
+    const [authData, setAuthData] = useState({
+        email: '',
+        password: '',
+        personId: ''
+    });
+
+    // Функция для обработки логина
+    const handleLogin = async (email, password, personId) => {
+        try {
+            let response = await loginModeus(email, password);
+
+            if (response.status === 200) {
+                setAuthData({email, password, personId});
+                localStorage.setItem('token', response.data["_netology-on-rails_session"]);
+                return {success: true};
+            } else {
+                return {success: false, message: "Неверный логин или пароль."};
+            }
+        } catch (error) {
+            return {success: false, message: "Произошла ошибка. Попробуйте снова."};
+        }
+    };
+
+    // Функция для поиска пользователя по ФИО
+    const handleSearch = async (fullName) => {
+        try {
+            let response = await searchModeus(fullName);
+
+            if (response.status === 200) {
+                return {success: true, data: response.data};
+            } else {
+                return {success: false, message: "Неверное ФИО. Попробуйте снова."};
+            }
+        } catch (error) {
+            return {success: false, message: "Произошла ошибка. Попробуйте снова."};
+        }
+    };
+
+    return (
+        <Router>
+            <Routes>
+                <Route path="/login" element={<LoginRoute onLogin={handleLogin} onSearch={handleSearch}/>}/>
+                <Route
+                    path="/calendar"
+                    element={
+                        <PrivateRoute>
+                            <CalendarRoute
+                                email={authData.email}
+                                password={authData.password}
+                                personId={authData.personId}
+                                token={localStorage.getItem('token')}
+                            />
+                        </PrivateRoute>
+                    }
+                />
+                {/* Дефолтный маршрут, который перенаправляет на /login */}
+                <Route path="*" element={<Navigate to="/login" />} />
+            </Routes>
+        </Router>
+    );
+};
+
+export default App;
