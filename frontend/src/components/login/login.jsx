b98f6d9ef1d088530adacfaaf0fe183ec47d2552
Echidden <julia.2252726@gmail.com>
2025-06-14 20:22:59 +0300

    upd
    доработка формы регистрации

--- frontend/src/components/login/login.jsx
+++ frontend/src/components/login/login.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 
-const Login = ({ onLogin, title, name }) => {
+const Login = ({ onLogin, title, name, formId }) => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
@@ -19,19 +19,33 @@ const Login = ({ onLogin, title, name }) => {
 
     return (
         <div className="login-netologiya">
+            <div class="loginLogo-container">
+                <center><img src={`/${formId}.png`} alt={`Лого ${name}`} class="loginLogo"/></center>
+            </div>
             <label>{title}</label>
             <div style={{ display: "flex", flexDirection: "column" }}>
                 <input
+                   type="hidden"
+                     name="login_system"
+                     value={formId}  
+                />
+                <input
                     className="input-email"
                     type="email"
+                    id={`email-${formId}`}
+                    name={`email-${formId}`}
                     placeholder={`Логин от ${name}`}
+                    autoComplete={`${formId}-email`}
                     onChange={(e) => setEmail(e.target.value)}
                     required
                 />
                 <input
                     className="input-email"
                     type="password"
+                    id={`password-${formId}`}
+                    name={`password-${formId}`}
                     placeholder={`Пароль от ${name}`}
+                    utoComplete={`${formId}-current-password`}
                     onChange={(e) => setPassword(e.target.value)}
                     required
                 />
